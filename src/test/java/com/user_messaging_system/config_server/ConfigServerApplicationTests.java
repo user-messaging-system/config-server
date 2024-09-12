425fb86e7585f0757d4de79eea160dfbc1eecc43
burakyapici747 <burakyapici747@gmail.com>
2024-09-12 23:23:39 +0300

    The fundamental infrastructure of the Config service has been created.

---
+++ src/test/java/com/user_messaging_system/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user_messaging_system.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
